f15fbb083392d2cf5998dc712fc9725599e5f755
Emil Cebotarosh <emil.cebotarosh@gmail.com>
2020-11-02 15:24:54 +0200

    Added initial daemon-like functionality

---
+++ src/server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import socket
+import threading
+import logging
+import sys
+
+logging.basicConfig(filename='../logs/server.log', level=logging.DEBUG)
+
+
+class ClientThread(threading.Thread):
+    def __init__(self,clientAddress,clientsocket, *args, **kwargs):
+        threading.Thread.__init__(self, *args, **kwargs)
+        self.csocket = clientsocket
+        logging.info("New connection added: {}".format(clientAddress))
+    def run(self):
+        logging.info("Connection from :{} ".format(clientAddress))
+        msg = ''
+        while True:
+            data = self.csocket.recv(2048)
+            msg = data.decode()
+            if msg=='bye':
+              break
+            print ("from client", msg)
+            self.csocket.send(bytes(msg,'UTF-8'))
+        logging.warning("Client at {} disconnected...".format(clientAddress))
+        sys.exit(0)
+        
+
+LOCALHOST = "CHANGEME"
+PORT = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((LOCALHOST, PORT))
+print("Server started")
+print("Waiting for client request..")
+while True:
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
+
