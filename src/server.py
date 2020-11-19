a0eaed26d4913e94c8870564cc7e45191bc95bc1
Emil Cebotarosh <emil.cebotarosh@gmail.com>
2020-11-19 22:10:26 +0200

    Made server log properly

--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@
 import socket
 import threading
 import logging
+import select
 import sys
 import os
 from dotenv import load_dotenv
@@ -15,19 +16,30 @@ class ClientThread(threading.Thread):
     def __init__(self,clientAddress,clientsocket, *args, **kwargs):
         threading.Thread.__init__(self, *args, **kwargs)
         self.csocket = clientsocket
+        self.clientAddress = clientAddress
+
         logging.info("New connection added: {}".format(clientAddress))
     def run(self):
         logging.info("Connection from :{} ".format(clientAddress))
         msg = ''
         while True:
-            data = self.csocket.recv(2048)
-            msg = data.decode()
-            if msg=='bye':
-              break
-            print ("from client", msg)
-            self.csocket.send(bytes(msg,'UTF-8'))
-        logging.warning("Client at {} disconnected...".format(clientAddress))
-        sys.exit(0)
+            if running:
+                r, _, _ = select.select([self.csocket], [], [], 1)
+                if r:
+                    data = self.csocket.recv(2048)
+                    msg = data.decode()
+                    if msg=='bye':
+                        logging.warning("Client at {} disconnected...".format(self.clientAddress))
+                        self.csocket.close()
+                        break
+                else:
+                    self.csocket.close()
+                    break
+                print ("from client", msg)
+                self.csocket.send(bytes(msg,'UTF-8'))
+            else:
+                break
+        
         
 
 IP = os.getenv("IP")
@@ -37,9 +49,24 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((IP, PORT))
 print("Server started")
 print("Waiting for client request..")
+threadPool = []
+running = True
 while True:
     server.listen(1)
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(clientAddress, clientsock)
-    newthread.start()
+    try:
+        clientsock, clientAddress = server.accept()
+        newthread = ClientThread(clientAddress, clientsock)
+        threadPool.append(newthread)
+        newthread.start()
+    except KeyboardInterrupt:
+        running=False
+        print("Setting running to False")
+        for thread in threadPool:
+            print("Proceeding to kill {}".format(thread))
+            thread.join()
+            print("Thread : {} joined".format(thread.clientAddress))
+        break
+
+print("Killing the server process")
+
 
