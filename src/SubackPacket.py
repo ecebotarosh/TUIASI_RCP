4de8806e2657d3a886fb826c8189c77642f8f194
Emil Cebotarosh <emil.cebotarosh@gmail.com>
2020-12-24 09:42:25 +0200

    Fixed faulty Suback implementation

--- src/SubackPacket.py
+++ src/SubackPacket.py
@@ -4,7 +4,7 @@ from MQTTPacket import MQTTPacket
 import struct
 from aux import CustomUTF8, VariableByte, BinaryData
 
-class ConnackPacket(MQTTPacket):
+class SubackPacket(MQTTPacket):
 	def __init__(self, data):
 		self.data=data
 		self.fixed={}
@@ -15,21 +15,29 @@ class ConnackPacket(MQTTPacket):
 		self.payload_size=0
 
 	@staticmethod
-	def generatePacketData(ack_flags : bool, reason_code: int, properties:dict, payload:dict) -> bytes:
-		data = b"\x90"
+	def generatePacketData(reason_code: int, properties:dict, payload:list) -> bytes:
+		fixed = b"\x90"
+		#TODO : add remaining length
+		
+		variable = struct.pack("!H", reason_code)
 		props = b""
-		if 'ReasonString' in properties.keys():
+		myProperties = properties.keys()
+		if 'ReasonString' in myProperties:
 			props += b"\x1F"+CustomUTF8.encode(properties['ReasonString'])
-		if 'UserProperty' in properties.keys():
+			myProperties.remove('ReasonString')
+		if 'UserProperty' in myProperties:
 			for key in properties['UserProperty'].keys():
 				for value in properties['UserProperty'][key]:
 					props+=b"\x26"+CustomUTF8.encode(key)+CustomUTF8.encode(value)
+			myProperties.remove('UserProperty')
+		variable+=VariableByte.encode(len(props))+props
+
+		remainingLength = VariableByte.encode(len(variable))
+		fixed+=remainingLength
+
 		payload_data = b""
-		for key in payload.keys():
-			payload_data += bytes(payload[key])
-		propertyLength = VariableByte.encode(len(props))
-		#len(propertyLength) pare stupid, dar e necesar
-		#propertyLength e format ca variable Byte si face parte din continutul pachetului.
-		remainingLength = VariableByte.encode(len(propertyLength)+len(props)+len(payload_data))
-		data += remainingLength + propertyLength + props + payload_data
-		return data
+
+		for response_reason_code in payload:
+			payload_data += struct.pack("!B", response_reason_code)
+
+		return fixed+variable+payload_data
