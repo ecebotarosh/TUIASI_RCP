7a88f3615b42aed37608f69b4107ea14cd41c6de
Emil Cebotarosh <emil.cebotarosh@gmail.com>
2021-01-07 10:51:57 +0200

    Fixed lacking security check

--- src/SubscribePacket.py
+++ src/SubscribePacket.py
@@ -16,6 +16,22 @@ class SubscribePacket(MQTTPacket):
         self.payload = {}
         self.payload_size = 0
 
+    def parseFixedHeader(self):
+        fixed_part = self.data[:1]
+	num=b""
+	for byte in self.data[1:]:
+	    num+=struct.pack("!B", byte)
+		if byte<0x80:
+		    break
+	required=len(num)
+	self.fixed['type'], self.fixed['remainingLength'] = struct.unpack("!B{}s".format(required), fixed_part+num)
+        if self.fixed['type'] & 0x0F != 0x02:
+            raise MQTTError("Malformed Packet")
+	self.fixed['type']>>=4
+	self.fixed['remainingLength']=VariableByte.decode(self.fixed['remainingLength'])
+	self.fixed_size=1+required
+ 
+
     def parseVariableHeader(self):
         variableHeader = self.data[self.fixed_size:]
 
