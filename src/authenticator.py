4c094f82f615c7065c15ff1aa6beb2739159b977
Emil Cebotarosh <emil.cebotarosh@gmail.com>
2021-01-17 22:09:05 +0200

    Implemented authentication

--- src/authenticator.py
+++ src/authenticator.py
@@ -2,18 +2,25 @@
 
 from config import Config
 
-
 class Authenticator:
     def __init__(self, config : Config):
         self.config = config
         self.clients = {}
+        self.getKnownCreds()
 
     def getKnownCreds(self):
-    	with open(self.config['KnownClientsPath']) as f:
+    	with open(self.config.config['KnownClientsPath']) as f:
     		self.known = f.readlines()
     		for client in self.known:
-    			splitted = client.split(:)
+    			splitted = client.split(":")
+    			self.clients[splitted[0]]=splitted[1].strip()
     			
+    def authenticate(self, username:str, password:bytes) -> bool:
+        return self.clients[username]==password.decode("utf-8")
 
-    def authenticate(self, username:str, password:BinaryData) -> bool
-
+if __name__=="__main__":
+    conf = Config()
+    conf.reload()
+    auth = Authenticator(conf)
+    print(auth.authenticate("admin", b'admin'))
+    print(auth.authenticate("admin", b'password'))
